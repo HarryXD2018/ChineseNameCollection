e2e49cd879360cf466eed4bfe4a6a8e404e26446
H.Chen <chenhj2000@163.com>
2020-06-01 22:12:35 +0800

    boost robust and add item to dataset

--- NameGenerator.py
+++ NameGenerator.py
@@ -11,18 +11,23 @@ class NameGenerator:
             self.data.append(temp)
 
     def check(self, row: list, name, family_name=None):
-        check1 = family_name is None        # 如果没有指定姓氏，就不需要check1
-        check2 = False
-        for item in row:
-            if name in item:
-                check2 = True
-                break
-        if family_name is not None and check2:
+        check1 = family_name is not None        # 姓氏验证
+        check2 = False                          # 字号验证
+        if (check1 is True) and (self.check_family_name(row, family_name) is False):    # 该行中不含所要姓氏
+            return False
+        else:
             for item in row:
-                if family_name in item:
-                    check1 = True
+                if name in item:
+                    check2 = True
                     break
-        return check1 and check2
+        return check2
+
+    def check_family_name(self, row: list, family_name:str):
+        for item in row:
+            if family_name in item:
+                return True
+        else:
+            return False
 
     def generate(self, name, family_name=None):
         print(name)
@@ -41,7 +46,7 @@ if __name__ == '__main__':
     print(name_gen.generate("东坡", family_name="苏"))
     print(name_gen.generate("苏东坡", family_name="苏"))
     print(name_gen.generate("六一居士"))
-    print(name_gen.generate("六一"))
+    print(name_gen.generate("放翁"))
 
 
 
